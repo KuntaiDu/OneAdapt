0e11b838fd3a0c1cd77915e026c6125e0c9d5a42
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2022-02-19 14:36:51 -0600

    Version before adding macroblock QPs

--- batch_chameleon.py
+++ batch_chameleon.py
@@ -4,35 +4,6 @@ from subprocess import run
 import os
 from pathlib import Path
 
-# qp_list = [1e-6,  0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-# qp_list = [0.95]
-# qp_list = qp_list[::-1]
-
-# qp_list = [qp_list[i] for i in range(len(qp_list)) if i % 2 == 0]
-# qp_list = [1]
-# qp_list = [1]
-# res_list = [1]
-# fr_list = [1e-6,  0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-# fr_list = [fr_list[i] for i in range(len(fr_list)) if i % 2 == 1]
-# res_list = [1e-6, 0.49, 1]
-# fr_list = res_list
-# qp_list = res_list
-
-res_list = [1]
-fr_list = [1]
-qp_list = [1]
-bwweight_list = [6]
-# res_list = [1e-6,  0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-# res_list = [res_list[i] for i in range(len(res_list)) if i % 2 == 1]
-
-lr = 0.2
-
-# lr = 0
-orig_freq = 1
-
-force = True
-
-
 
 def probe_range(fmt):
     
@@ -56,25 +27,24 @@ for fmt in fmts:
 
     approach = f'chameleon'
 
-    if force or not os.path.exists(output):
 
-        run([
-            'python', 'chameleon.py',
-            '-i', fmt,
-            # '-i', 'videos/yoda/dashcam_1/part%d.mp4',
-            # '--sec', '61',
-            '--start', '0',
-            '--end', '%d' % probe_range(fmt),
-            '--frequency', '100',
-            # '--qp', f'{qp}',
-            # '--res', f'{res}',
-            # '--fr', f'{fr}',
-            # '--lr', f'{lr}',
-            # '--freq', f'{freq}',
-            # '--train',
-            '--approach', approach,
-            # '--bw_weight', f'{bwweight}',
-        ])
+    run([
+        'python', 'chameleon.py',
+        '-i', fmt,
+        # '-i', 'videos/yoda/dashcam_1/part%d.mp4',
+        # '--sec', '61',
+        '--start', '0',
+        '--end', '%d' % probe_range(fmt),
+        '--frequency', '100',
+        # '--qp', f'{qp}',
+        # '--res', f'{res}',
+        # '--fr', f'{fr}',
+        # '--lr', f'{lr}',
+        # '--freq', f'{freq}',
+        # '--train',
+        '--approach', approach,
+        # '--bw_weight', f'{bwweight}',
+    ])
 
     # freq = 1
 
